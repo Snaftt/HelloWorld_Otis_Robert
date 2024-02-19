127f03797182f76eefa3a862aedd8670857d1249
Good morning yawl <robertc.otis791@gmail.com>
2024-02-19 09:33:41 -0600

    Hello world
    completed.

--- HelloWorld_Otis_Robert/Program.cs
+++ HelloWorld_Otis_Robert/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+{
+    Console.WriteLine("What is your first name?"); // Asks you for your first name.
+    string firstName = Console.ReadLine(); // Reads your response.
+
+    Console.WriteLine("What is your last name"); //Asks you for your last name.
+    string lastName = Console.ReadLine(); // Reads your response.
+
+    Console.WriteLine($"Hello {firstName} {lastName}"); // Shows your frist and last name..
+}
